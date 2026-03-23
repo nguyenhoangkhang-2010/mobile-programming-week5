abac81d16d693a8fb44cf630ec95cb7f10afc3a7
nguyenhoangkhang-2010 <nguyenhoangkhang.241105@gmail.com>
2026-03-23 12:34:29 +0700

    add file models/feature_model

---
+++ lib/models/feature_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FeatureModel {
+  final String title;
+  final IconData icon;
+  final Color color;
+
+  FeatureModel({
+    required this.title,
+    required this.icon,
+    required this.color,
+  });
+}
